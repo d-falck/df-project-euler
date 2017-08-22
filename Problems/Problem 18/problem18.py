39d272cfd1381a2221ecc52a4adb96db6caaa2eb
Damon Falck <damon.falck@icloud.com>
2017-08-22 17:08:16 +0100

    Convert string entries to int

--- Problems/Problem 18/problem18.py
+++ Problems/Problem 18/problem18.py
@@ -8,13 +8,13 @@ maxsum = [[0 for entry in row] for row in triangle] # Zeroes same size as triang
 for i, row in enumerate(triangle):
     for j, entry in enumerate(row):
         if i == 0:
-            maxsum[i][j] = entry
+            maxsum[i][j] = int(entry)
         if j == 0: # If at the start of the line, only one option for max sum
-            maxsum[i][j] = entry + maxsum[i-1][j]
+            maxsum[i][j] = int(entry) + maxsum[i-1][j]
         elif j == len(row) - 1: # If at the end of the line, only one option
-            maxsum[i][j] = entry + maxsum[i-1][j-1]
+            maxsum[i][j] = int(entry) + maxsum[i-1][j-1]
         else: # Normally, compare the two numbers directly above entry and add the largest to the maxsum here
             if maxsum[i-1][j-1] > maxsum[i-1][j]:
-                maxsum[i][j] = entry + maxsum[i-1][j-1]
+                maxsum[i][j] = int(entry) + maxsum[i-1][j-1]
             else:
-                maxsum[i][j] = entry + maxsum[i-1][j]
+                maxsum[i][j] = int(entry) + maxsum[i-1][j]
