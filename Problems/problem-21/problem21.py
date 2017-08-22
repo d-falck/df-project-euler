bd41a002eff99d8fdf873c9d1afaea049ca8e688
Damon Falck <damon.falck@icloud.com>
2017-08-22 21:29:02 +0100

    finished p21

--- Problems/problem-21/problem21.py
+++ Problems/problem-21/problem21.py
@@ -1,19 +1,31 @@
 import math
 
-def findFactors(n): # Ported roughly from problem 12
+def properDivisors(n): # Ported roughly from problem 12
     factors = []
     for i in range(1,math.floor(math.sqrt(n))+1):
         if n % i == 0:
-            if i not in factors:
+            if i not in factors and i < n:
                 factors.append(i)
-            if int(n/i) not in factors:
+            if int(n/i) not in factors and int(n/i) < n:
                 factors.append(int(n/i))
     return factors
 
-allFactors = [0]
+checkUnder = 10000
 
-for i in range(1,10001): # so that allFactors[N] is the list of factors of N
-    allFactors.append(findFactors(i))
-print(allFactors)
+factorSums = [0 for i in range(checkUnder)]
+amicables = []
 
-for entry in allFactors:
+for i in range(1,checkUnder): # so that factorSums[N] is the sum of proper divisors of N
+    # print(i)
+    factorSums[i] = sum(properDivisors(i))
+
+for i in range(1,checkUnder):
+    # print(i)
+    for j in range(1,checkUnder):
+        if factorSums[i] == j and factorSums[j] == i and i != j:
+            if i not in amicables or j not in amicables:
+                amicables.append(i)
+                amicables.append(j)
+
+print(amicables)
+print(sum(amicables))
