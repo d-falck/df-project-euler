4daa9b074a46fa4149cdcf89e02f25494747cc2c
Damon Falck <damon.falck@icloud.com>
2017-08-22 18:15:39 +0100

    started on problem 19

---
+++ Problems/problem-19/problem19.py
@@ -0,0 +1,16 @@
+firstSundays = 0
+
+day = # Every day is given a number, indexed from 0 at 1 Jan 1900
+date = 0 # Date of the month (0-30)
+month = 0 # Current working month (0-11)
+year = 1900 # Current year
+
+while True:
+    if month in [3,5,8,10]: # If current month has 30 days
+        if date >=
+    elif month == 1: # If February
+        pass
+    else: # Current month has 31 days
+        pass
+
+    day += 1
