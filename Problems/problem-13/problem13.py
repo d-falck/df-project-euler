c892f39d65b645ec190c0989ab5ddcc4cf384c96
Damon Falck <damon.falck@icloud.com>
2017-08-24 00:28:31 +0100

    add and complete problem 13 (very quick) this means all problems are intact now so far

--- Problems/problem-13/problem13.py
+++ Problems/problem-13/problem13.py
@@ -1,1 +1,6 @@
-# Needs a copy and paste
+with open('input.txt') as f:
+    numbers = [int(line.strip()[0:13]) for line in f]
+
+summation = sum(numbers)
+first10 = int(str(summation)[0:10])
+print(first10)
