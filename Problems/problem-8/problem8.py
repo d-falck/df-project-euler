7116094b1654e8ef9b3915544d5ca898507cd731
Damon Falck <damon.falck@icloud.com>
2017-08-23 23:35:09 +0100

    done problem 8 (easy now)

--- Problems/problem-8/problem8.py
+++ Problems/problem-8/problem8.py
@@ -1,2 +1,19 @@
+def prod(iterable):
+    product = 1
+    for i in iterable:
+        product *= i
+    return product
+
 with open('input.txt') as f:
     data = f.read().replace('\n','')
+
+digitsLength = 13 # The number of adjacent digits we want to find the product of
+maxProduct = 0
+
+for i in range(len(data)-digitsLength):
+    digits = [int(j) for j in data[i:i+digitsLength]]
+    product = prod(digits)
+    if product > maxProduct:
+        maxProduct = product
+
+print(maxProduct)
