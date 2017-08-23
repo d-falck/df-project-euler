d8354657703f7a5f14ed6d49271bf8637d449f3c
Damon Falck <damon.falck@icloud.com>
2017-08-23 03:10:20 +0100

    attempted dynamic solution; failed to include single previous abundants, also still not efficient

--- Problems/problem-23/problem23.py
+++ Problems/problem-23/problem23.py
@@ -25,10 +25,24 @@ for i in range(1,checkBelow+1):
 
 # We now have all the abundant numbers under checkBelow in the list abundants
 
-abundantSums = []
-for abundant1 in abundants:
-    for abundant2 in abundants:
-        if abundant1 + abundant2 not in abundantSums:
-            abundantSums.append(abundant1 + abundant2)
+possibleSums = [0]
+# Since sums of ABCDE are the sums of ABCD without E and the sums of ABCD with E
+# For each abundant number we calculate all possible sums with previous ones, adding any new ones to the list
+for abundant in abundants:
+    print(abundant)
+    for previousPossibleSum in possibleSums:
+        newSum = previousPossibleSum + abundant
+        if newSum not in possibleSums:
+            possibleSums.append(newSum)
+print(possibleSums)
+# Now we make a list of all the numbers not in possibleSums
+impossibleSums = []
+for i in range(1,checkBelow+1):
+    if i not in possibleSums and i not in impossibleSums:
+        impossibleSums.append(i)
 
-print(abundantSums)
+# Our final result:
+# print("Possible sums:")
+# print(possibleSums)
+# print("Impossible sums")
+# print(impossibleSums)
