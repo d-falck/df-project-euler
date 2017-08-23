c1f858078d0c87693692f494ac014ed806a7bf2a
Damon Falck <damon.falck@icloud.com>
2017-08-23 23:59:50 +0100

    done a solution to p11, failed

--- Problems/problem-11/problem11.py
+++ Problems/problem-11/problem11.py
@@ -1,1 +1,37 @@
-# Again requires copy and paste
+def prod(iterable):
+    product = 1
+    for i in iterable:
+        product *= i
+    return product
+
+with open('input.txt') as f:
+    grid = [[int(i) for i in line.replace('\n','').split(' ')] for line in f]
+
+width = len(grid[0])
+height = len(grid)
+
+maxProduct = 0
+adjacents = 4
+
+# Vertical adjacents
+for i in range(height - adjacents + 1):
+    for j in range(width):
+        product = prod([grid[i+k][j] for k in range(adjacents)])
+        if product > maxProduct:
+            maxProduct = product
+
+# Horizontal adjacents
+for i in range(height):
+    for j in range(width - adjacents + 1):
+        product = prod([grid[i][j+k] for k in range(adjacents)])
+        if product > maxProduct:
+            maxProduct = product
+
+# Diagonal adjacents
+for i in range(height - adjacents + 1):
+    for j in range(width - adjacents + 1):
+        product = prod([grid[i+k][j+k] for k in range(adjacents)])
+        if product > maxProduct:
+            maxProduct = product
+
+print(maxProduct)
