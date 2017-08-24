512e7d287ca9c847cdbc7a3eed499a71a99c4d3e
Damon Falck <damon.falck@icloud.com>
2017-08-24 20:32:44 +0100

    optimised problem 3

--- Problems/problem-3/problem3.py
+++ Problems/problem-3/problem3.py
@@ -1,32 +1,27 @@
 import math
 
-def isprime(number):
-    prime = True #Assume prime
+def isPrime(number):
     if number == 1:
         return False
-    if number == 2:
-            return True
+    elif number == 2:
+        return True
     else:
         for i in range(2,math.ceil(math.sqrt(number))+1):
             if number % i == 0:
                 return False
-        return True
+    return True
 
-def primefactors(number):
+def primeFactors(number):
     primefactors = []
-    keep_going = True
-    while keep_going:
-        keep_going = False
-        for i in range(1,math.ceil(math.sqrt(number))+1): # Only check up to the square root: any other factors can be found from the existing ones
-            if number % i == 0:
-                if isprime(i):
-                    primefactors.append(i)
-                    number = number/i
-                    keep_going = True
-                    break
-        if isprime(number):
+    while True:
+        if isPrime(number):
             primefactors.append(number)
             break
+        for i in range(1,math.ceil(math.sqrt(number))+1): # Only check up to the square root: any other factors can be found from the existing ones
+            if number % i == 0 and isPrime(i):
+                primefactors.append(i)
+                number = int(number/i)
+                break
     return primefactors
 
-print(max(primefactors(600851475143)))
+print(max(primeFactors(600851475143)))
